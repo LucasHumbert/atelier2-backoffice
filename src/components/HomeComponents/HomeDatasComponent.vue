4d6d2cc535bbdb3a62d900cfdcc19853d0b2d684
Lucas Humbert <lucas.humbert.28@gmail.com>
2022-03-21 18:13:54 +0100

    :sparkles: fonctionnalités de base

---
+++ src/components/HomeComponents/HomeDatasComponent.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="box columns">
+  <div class="column is-4 has-text-centered">
+    <span class="is-size-2">{{ nbEvents }}</span>
+    <p class="mt-3 is-size-5">événements crées</p>
+  </div>
+  <div class="column is-4 has-text-centered">
+    <span class="is-size-2">{{ nbUsers }}</span>
+    <p class="mt-3 is-size-5">utilisateurs inscrits</p>
+  </div>
+  <div class="column is-4 has-text-centered">
+    <span class="is-size-2">{{ nbMessages }}</span>
+    <p class="mt-3 is-size-5">messages postés</p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "HomeDatasComponent",
+  data() {
+    return {
+      nbEvents: 80,
+      nbUsers: 192,
+      nbMessages: 265
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Users.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Liste des utilisateurs</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UsersView"
+}
+</script>
+
+<style scoped>
+
+</style>
