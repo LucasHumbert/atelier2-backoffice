41d11da04e62a3b1e33c4cb78b9b51a82725c62c
Lucas Humbert <lucas.humbert.28@gmail.com>
2022-03-27 17:35:09 +0200

    :sparkles: added button deconnection on error page

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,12 +1,24 @@
 <template>
-<div>
+<div class="has-text-centered">
   <h1 class="title is-size-2 has-text-centered">Unauthorized</h1>
+  <b-button type="is-danger"
+            icon-pack="fa-solid"
+            icon-right="right-to-bracket"
+            @click="deconnection">
+    Deconnexion
+  </b-button>
 </div>
 </template>
 
 <script>
 export default {
-  name: "ErrorView"
+  name: "ErrorView",
+  methods: {
+    deconnection(){
+      this.$store.commit('setToken', false)
+      this.$router.push('/connection')
+    }
+  }
 }
 </script>
 
